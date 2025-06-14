7a323070c30a2fbe035031b46dec8a89119bb78b
Ziggy-os <nimsarawhite@gmail.com>
2025-06-15 00:45:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "𝚁𝙰𝚅𝙸𝙽𝙳𝚄-𝙼𝙳=y9g0FAaI#k-dO5zM6iYZAV0521JANpQRiy53dQiFn88mvY1E74oY",// session id 💙
+HACKER : process.env.HACKER || "94768345057" //Addmin number 💙
 };
